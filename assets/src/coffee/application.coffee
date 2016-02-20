08c405ba5bf2869d11148f2dd34fe3958b2f1790
kotaro_kimura <kotaro_k@icloud.com>
2016-02-20 12:30:37 +0900

    物体が動くまで

--- assets/src/coffee/application.coffee
+++ assets/src/coffee/application.coffee
@@ -1,34 +1,65 @@
-# グローバル変数を宣言
+# グローバル変数
 exports = this
-exports.global_object = {
-  color: {
-    black: '#000',
-    white: '#fff'
-  },
-  field: {
-    width: 980,
-    height: 500
-  }
+exports.globalObject = {
+  field : { width: 980, height: 500 }
   canvas: $('canvas#field')
 }
 
 class Field
   draw: ->
-    exports.global_object['canvas'][0].width = exports.global_object['field']['width']
-    exports.global_object['canvas'][0].height = exports.global_object['field']['height']
+    exports.globalObject['canvas'][0].width = exports.globalObject['field']['width']
+    exports.globalObject['canvas'][0].height = exports.globalObject['field']['height']
 
 class Player
+  constructor: ->
+    @radius = 10
+    @width  = (exports.globalObject['field']['width'] - @radius) / 2
+    @height = exports.globalObject['field']['height'] - 20
+
   draw: ->
-    radius = 10
-    exports.global_object['canvas'].drawArc(
+    exports.globalObject['canvas'].drawArc(
       {
-        fillStyle: exports.global_object['color']['white'],
-        x: (exports.global_object['field']['width'] - radius) / 2,
-        y: exports.global_object['field']['height'] - 20,
-        radius: radius
+        fillStyle: '#fff',
+        x: @width,
+        y: @height,
+        radius: @radius
       }
     )
 
+  clear: ->
+    exports.globalObject['canvas'].clearCanvas()
+
+class Movement
+  DISTANCE = 5
+  KEYDOWN = { left: 37, up: 38, right: 39, down: 40 }
+  constructor: (@keyCode) ->
+
+  doMove: ->
+    if _pushedArrowKey.call @
+      _controlMovingDistance.call @
+      exports.globalObject['player'].clear()
+      exports.globalObject['player'].draw()
+
+  _controlMovingDistance = ->
+    switch @keyCode
+      when KEYDOWN['left']
+        exports.globalObject['player'].width -= DISTANCE
+      when KEYDOWN['up']
+        exports.globalObject['player'].height -= DISTANCE
+      when KEYDOWN['right']
+        exports.globalObject['player'].width += DISTANCE
+      when KEYDOWN['down']
+        exports.globalObject['player'].height += DISTANCE
+
+  _pushedArrowKey = ->
+    unless $.inArray(
+      @keyCode, [KEYDOWN['left'], KEYDOWN['up'], KEYDOWN['right'], KEYDOWN['down']]
+    ) == -1 then true else false
+
 do ->
   new Field().draw()
-  new Player().draw()
+  exports.globalObject['player'] = new Player()
+  exports.globalObject['player'].draw()
+
+$(document).on 'keydown', 'body', (e) ->
+  new Movement(e.keyCode).doMove()
--- assets/src/js/application.js
+++ assets/src/js/application.js
@@ -1,53 +1,106 @@
-(function() {
-  var Field, Player, exports;
+var Field, Movement, Player, exports;
 
-  exports = this;
+exports = this;
 
-  exports.global_object = {
-    color: {
-      black: '#000',
-      white: '#fff'
-    },
-    field: {
-      width: 980,
-      height: 500
-    },
-    canvas: $('canvas#field')
+exports.globalObject = {
+  field: {
+    width: 980,
+    height: 500
+  },
+  canvas: $('canvas#field')
+};
+
+Field = (function() {
+  function Field() {}
+
+  Field.prototype.draw = function() {
+    exports.globalObject['canvas'][0].width = exports.globalObject['field']['width'];
+    return exports.globalObject['canvas'][0].height = exports.globalObject['field']['height'];
   };
 
-  Field = (function() {
-    function Field() {}
+  return Field;
 
-    Field.prototype.draw = function() {
-      exports.global_object['canvas'][0].width = exports.global_object['field']['width'];
-      return exports.global_object['canvas'][0].height = exports.global_object['field']['height'];
-    };
+})();
 
-    return Field;
+Player = (function() {
+  function Player() {
+    this.radius = 10;
+    this.width = (exports.globalObject['field']['width'] - this.radius) / 2;
+    this.height = exports.globalObject['field']['height'] - 20;
+  }
 
-  })();
+  Player.prototype.draw = function() {
+    return exports.globalObject['canvas'].drawArc({
+      fillStyle: '#fff',
+      x: this.width,
+      y: this.height,
+      radius: this.radius
+    });
+  };
 
-  Player = (function() {
-    function Player() {}
+  Player.prototype.clear = function() {
+    return exports.globalObject['canvas'].clearCanvas();
+  };
 
-    Player.prototype.draw = function() {
-      var radius;
-      radius = 10;
-      return exports.global_object['canvas'].drawArc({
-        fillStyle: exports.global_object['color']['white'],
-        x: (exports.global_object['field']['width'] - radius) / 2,
-        y: exports.global_object['field']['height'] - 20,
-        radius: radius
-      });
-    };
+  return Player;
 
-    return Player;
+})();
 
-  })();
+Movement = (function() {
+  var DISTANCE, KEYDOWN, _controlMovingDistance, _pushedArrowKey;
 
-  (function() {
-    new Field().draw();
-    return new Player().draw();
-  })();
+  DISTANCE = 5;
 
-}).call(this);
+  KEYDOWN = {
+    left: 37,
+    up: 38,
+    right: 39,
+    down: 40
+  };
+
+  function Movement(keyCode) {
+    this.keyCode = keyCode;
+  }
+
+  Movement.prototype.doMove = function() {
+    if (_pushedArrowKey.call(this)) {
+      _controlMovingDistance.call(this);
+      exports.globalObject['player'].clear();
+      return exports.globalObject['player'].draw();
+    }
+  };
+
+  _controlMovingDistance = function() {
+    switch (this.keyCode) {
+      case KEYDOWN['left']:
+        return exports.globalObject['player'].width -= DISTANCE;
+      case KEYDOWN['up']:
+        return exports.globalObject['player'].height -= DISTANCE;
+      case KEYDOWN['right']:
+        return exports.globalObject['player'].width += DISTANCE;
+      case KEYDOWN['down']:
+        return exports.globalObject['player'].height += DISTANCE;
+    }
+  };
+
+  _pushedArrowKey = function() {
+    if ($.inArray(this.keyCode, [KEYDOWN['left'], KEYDOWN['up'], KEYDOWN['right'], KEYDOWN['down']]) !== -1) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  return Movement;
+
+})();
+
+(function() {
+  new Field().draw();
+  exports.globalObject['player'] = new Player();
+  return exports.globalObject['player'].draw();
+})();
+
+$(document).on('keydown', 'body', function(e) {
+  return new Movement(e.keyCode).doMove();
+});
